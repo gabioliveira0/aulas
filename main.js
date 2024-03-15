476163c7068a89dc7c8ba50f14f7cb583cc0177f
gabioliveira0 <162706091+gabioliveira0@users.noreply.github.com>
2024-03-15 10:52:18 -0300

    trabalho

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes);
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclik=function(){
+        botoes[i]
+    }
+    console.log()
+}
